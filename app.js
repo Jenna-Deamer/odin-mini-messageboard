7e6c2107711f6bc37613629fd13e62f80965c3c0
Jenna Deamer <jennadeamer@gmail.com>
2025-12-15 16:44:26 -0500

    Add messages to indexView, Add basic styles, Add index router

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const path = require('node:path');
+const express = require("express");
+const path = require("node:path");
+
 const app = express();
 
-app.set('views', path.join(__dirname,'views'))
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-const assetsPath = path.join(__dirname, "public");
-app.use(express.static(assetsPath));
 
-app.get('/', (req, res) =>{
-    res.render('index')
-})
+app.use(express.static(path.join(__dirname, "public")));
+
+const indexRouter = require("./routes/indexRouter");
+app.use("/", indexRouter);
+
+app.get("/new", (req, res) => {
+  res.render("newMessage");
+});
 
 const PORT = 3000;
-app.listen(PORT, (error) =>{
-    if(error){
-        throw error;
-    }
-    console.log(`Express app listening on ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Express app listening on ${PORT}`);
+});
