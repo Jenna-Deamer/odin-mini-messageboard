6d9c2591a2f138063914e73cc6bcf52bae39cbe0
Jenna Deamer <jennadeamer@gmail.com>
2025-12-15 18:01:52 -0500

    Add id to all messages, Add details view for messages,

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -6,5 +6,13 @@ indexRouter.get('/', (req, res) =>{
     res.render('index', {title: 'Mini Messageboard', messages: messages})
 })
 
+indexRouter.get("/messageDetails/:id", (req,res) =>{
+    const id = Number(req.params.id);
+
+    if(Number.isNaN(id) || !messages[id]){
+        return res.status(404).send('404 - Message not found');
+    }
+    res.render("messageDetails", {message: messages[id], id})
+})
 
 module.exports = indexRouter;
